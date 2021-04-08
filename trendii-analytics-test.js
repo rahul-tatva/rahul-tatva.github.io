5663cccef398d45ccc87125728a3f4a98091dca0
Rahul Parmar <rahulparmar.n@gmail.com>
2021-04-08 13:23:06 +0530

    revised

--- trendii-analytics-test.js
+++ trendii-analytics-test.js
@@ -7,7 +7,25 @@
  */
 // references:
 //stackoverflow.com/questions/17628456/measure-distance-between-two-html-elements-centers
+function getPositionAtCenter(element) {
+  const { top, left, width, height } = element.getBoundingClientRect();
+  return {
+    x: left + width / 2,
+    y: top + height / 2,
+  };
+}
 
+function getDistanceBetweenElements(a, b) {
+  const aPosition = getPositionAtCenter(a);
+  const bPosition = getPositionAtCenter(b);
+
+  return Math.hypot(aPosition.x - bPosition.x, aPosition.y - bPosition.y);
+}
+
+const distance = getDistanceBetweenElements(
+  document.getElementById("x"),
+  document.getElementById("y")
+);
 document.addEventListener("DOMContentLoaded", function handleDOMLoaded() {
   console.log("anaylitics test");
   const adContainerWithNearestImageData = [];
@@ -28,8 +46,10 @@ document.addEventListener("DOMContentLoaded", function handleDOMLoaded() {
   for (var i = 0; i < imageCollection.length; i++) {
     const imageEl = imageCollection[i];
     const imgElSrc = imageCollection[i].src;
-    const distance = mezr.distance(adContainerEl, imageEl);
-    const distance2 = mezr.distance(adContainerFrameEl, imageEl);
+    // const distance = mezr.distance(adContainerEl, imageEl);
+    // const distance2 = mezr.distance(adContainerFrameEl, imageEl);
+    const distance = getDistanceBetweenElements(adContainerEl, imageEl);
+    const distance2 = getDistanceBetweenElements(adContainerFrameEl, imageEl);
     const imageData = {
       imageSrc: imgElSrc,
       distance: distance,
