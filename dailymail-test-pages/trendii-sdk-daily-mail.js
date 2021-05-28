a1dea175110a6a49279871c18aab62418c8fd3a0
Rahul Parmar <rahulparmar.n@gmail.com>
2021-05-28 13:45:42 +0530

    revised

--- dailymail-test-pages/trendii-sdk-daily-mail.js
+++ dailymail-test-pages/trendii-sdk-daily-mail.js
@@ -125,6 +125,90 @@ window.FEED_PRODUCTS = {
           "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/33/c317b9a054040539f4e2a6a4a75a83d5.jpg",
           "url": "https://trendii.com/dress/needle--thread-sweet-petal-embroidered-dress--white/d74bf41dc704cc218a7d726eb793e443",
           "currency": "£"
+        },
+        {
+          "id": 4827204,
+          "price": "920.00",
+          "name": "Needle & Thread - Sweet Petal embroidered dress - women - Polyester - 6 - White",
+          "cashback": "4%",
+          "sale": false,
+          "category": "dress",
+          "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/310/ef5c49bfad066ac9199016a5a872bf97.jpg",
+          "url": "https://trendii.com/dress/needle--thread--sweet-petal-embroidered-dress--women--polyester--6--white/dcd5c84004e376860262256ffbf0fe9f",
+          "currency": "$"
+        },
+        {
+          "name": "Levi's turned hem denim shorts - White",
+          "price": "102.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "£",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=4151575&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Flevis-turned-hem-denim-shorts--white%2F983e3a1c9ee2d82afba6b929196fc46a",
+          "image": "https://cdn.trendii.com/assets/1.jpg"
+        },
+        {
+          "name": "Acne Studios - high-rise denim shorts - women - Cotton - 25, 26 - Blue",
+          "price": "320.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "$",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=7470268&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Facne-studios--highrise-denim-shorts--women--cotton--25-26--blue%2F1a6577625b11ccbf1606da90eab40d3b",
+          "image": "https://cdn.trendii.com/assets/2.jpg"
+        },
+        {
+          "name": "FRAME cutoff shorts - White",
+          "price": "286.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "£",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=4151674&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Fframe-cutoff-shorts--white%2F5f2df18200d959ebbc0244afc694ffa3",
+          "image": "https://cdn.trendii.com/assets/3.jpg"
+        },
+        {
+          "name": "MAISON MARGIELA Shorts - Item 13301120",
+          "price": "569.40",
+          "sale": true,
+          "category": "shorts",
+          "currency": "$",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=8338005&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Fmaison-margiela-shorts--item-13301120%2F37777f125f61b7a648e938715d690ecf",
+          "image": "https://cdn.trendii.com/assets/4.jpg"
+
+        },
+        {
+          "name": "Levi's turned hem denim shorts - White",
+          "price": "102.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "£",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=4151575&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Flevis-turned-hem-denim-shorts--white%2F983e3a1c9ee2d82afba6b929196fc46a",
+          "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/33/1f43e1d522e530c8c627fe5995d53c09.jpg"
+        },
+        {
+          "name": "Acne Studios - high-rise denim shorts - women - Cotton - 25, 26 - Blue",
+          "price": "320.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "$",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=7470268&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Facne-studios--highrise-denim-shorts--women--cotton--25-26--blue%2F1a6577625b11ccbf1606da90eab40d3b",
+          "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/310/607401445c51e9a8e60c9957437a3595.jpg"
+        },
+        {
+          "name": "FRAME cutoff shorts - White",
+          "price": "286.00",
+          "sale": false,
+          "category": "shorts",
+          "currency": "£",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=4151674&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Fframe-cutoff-shorts--white%2F5f2df18200d959ebbc0244afc694ffa3",
+          "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/33/29f2196d2e992c2bb6e0ea932920de99.jpg"
+        },
+        {
+          "name": "MAISON MARGIELA Shorts - Item 13301120",
+          "price": "569.40",
+          "sale": true,
+          "category": "shorts",
+          "currency": "$",
+          "url": "https://affiliate.trendii.com/affiliate-link?affiliateId=TRENDII&utm_source=12345&utm_campaign=12345&utm_term=8338005&utm_content=12345&utm_medium=12345&redirectUrl=https%3A%2F%2Ftrendii.com%2Fshorts%2Fmaison-margiela-shorts--item-13301120%2F37777f125f61b7a648e938715d690ecf",
+          "image": "https://storage.googleapis.com/trendii-products/__trendii__images__/129/3fb71ff836dbbebb786d1523041bd803.JPG"
         }
       ]
     },
