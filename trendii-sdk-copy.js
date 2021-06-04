d33e41087d6d11daadcc0c4b5ac8b7f9ccc8819d
Rahul Parmar <rahulparmar.n@gmail.com>
2021-06-04 13:23:42 +0530

    revised

--- trendii-sdk-copy.js
+++ trendii-sdk-copy.js
@@ -602,7 +602,7 @@ class TRENDiiAd {
     if (!!window.IntersectionObserver) {
       const options = {
         rootMargin: "0px 0px 0px 0px",
-        threshold: 0.30,
+        threshold: 0.5,
       };
       this.intersectionObserver = new IntersectionObserver(
         this.handleIntersectionEntries.bind(this),
