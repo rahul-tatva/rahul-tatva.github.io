e1e5f196da55cdf9299379fd4a93acffe69defca
Rahul Parmar <rahulparmar.n@gmail.com>
2021-06-09 19:50:34 +0530

    revised

--- dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
+++ dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
@@ -535,6 +535,8 @@ class TRENDiiAd {
     this.loadScript("https://cdn.trendii.com/assets/splide.min.js");
     this.loadStyleSheet("https://cdn.trendii.com/assets/splide-core.min.css");
     this.loadStyleSheet("https://rahul-tatva.github.io/sdk-html-templates/daily-mail.css");
+    this.loadStyleSheet("https://rahul-tatva.github.io/sdk-html-templates/daily-mail-all-product.css");
+
     // this.loadStyleSheet("https://rahul-tatva.github.io/sdk-html-templates/Products-Silder.css");
     // this.loadScript("https://unpkg.com/axios/dist/axios.min.js");
     // <link rel="stylesheet" href="./sdk-html-templates/daily-mail.css"></link>
