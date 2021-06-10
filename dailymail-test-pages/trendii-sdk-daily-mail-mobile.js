dbb41730a741ad987adadb571fcbe1c7acb0b3c3
Rahul Parmar <rahulparmar.n@gmail.com>
2021-06-10 16:08:31 +0530

    revised

--- dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
+++ dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
@@ -738,8 +738,8 @@ class TRENDiiAd {
     // to test 1-2-3-4 products case
     // const products = imageData.products.splice(0, (index % 2 === 0 ? 2 : 1));
     // for 3 and 4 products
-    const products = imageData.products.splice(0, (index % 2 === 0 ? 3 : 4));
-    // const products = imageData.products;
+    // const products = imageData.products.splice(0, (index % 2 === 0 ? 3 : 4));
+    const products = imageData.products;
     const advertiserName = imageData.advertiserName;
     const identifier = `splide${index}`;
     imageData.sliderId = identifier;
