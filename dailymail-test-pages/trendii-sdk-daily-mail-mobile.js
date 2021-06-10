1b002a070db47a371c689d81ed4d0a8c31e0b59b
Rahul Parmar <rahulparmar.n@gmail.com>
2021-06-10 18:49:34 +0530

    revised

--- dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
+++ dailymail-test-pages/trendii-sdk-daily-mail-mobile.js
@@ -577,23 +577,23 @@ class TRENDiiAd {
     // document.addEventListener("DOMContentLoaded", this.handleDOMLoaded.bind(this));
     // window.addEventListener("load", () => { });
 
-    window.addEventListener("load", () => {
-      console.log("DOM is ready");
-      //debugger;
-      this.getAllDailyMailBlogImagesFromDOM();
-      const requestOptions = { method: "GET" };
+    // window.addEventListener("load", () => {
+    //   console.log("DOM is ready");
+    //   //debugger;
+    //   this.getAllDailyMailBlogImagesFromDOM();
+    //   const requestOptions = { method: "GET" };
 
-      Promise.all([
-        fetch(this.API_GET_NATIVE_AD_SLIDER_TEMPLATE).then((response) => response.text()),
-        fetch(this.API_GET_NATIVE_AD_SIMPLE_TEMPLATE).then((response) => response.text()),
-      ]).then(allResponses => {
-        this.nativeAdSliderTemplateHTMLString = allResponses[0];
-        this.nativeAdSimpleTemplateHTMLString = allResponses[1];
-        // const response3 = allResponses[2];
-        console.log("templates are ready");
-        this.getProductsForAllImages();
-      });
-    });
+    //   Promise.all([
+    //     fetch(this.API_GET_NATIVE_AD_SLIDER_TEMPLATE).then((response) => response.text()),
+    //     fetch(this.API_GET_NATIVE_AD_SIMPLE_TEMPLATE).then((response) => response.text()),
+    //   ]).then(allResponses => {
+    //     this.nativeAdSliderTemplateHTMLString = allResponses[0];
+    //     this.nativeAdSimpleTemplateHTMLString = allResponses[1];
+    //     // const response3 = allResponses[2];
+    //     console.log("templates are ready");
+    //     this.getProductsForAllImages();
+    //   });
+    // });
 
     document.addEventListener("DOMContentLoaded", () => {
       console.log("DOM is ready");
