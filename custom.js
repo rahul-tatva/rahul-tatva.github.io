e866e8d32419faade8d93746a99ce050ffaac223
rahul-tatva <61497462+rahul-tatva@users.noreply.github.com>
2021-09-08 18:51:33 +0530

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const datePickerRange = document.getElementById('date-picker-range');
+    debugger
+    console.log("Testetsets");
+   alert("test")
+});
+
+window.addEventListener('load', () => {
+    const datePickerRange = document.getElementById('date-picker-range');
+        debugger
+
+//   alert("test")
+});
